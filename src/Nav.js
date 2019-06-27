b05e4d4f97f922a36bd18b54a4fe5ff3010c8dc5
puneetn912 <naikpuneetgs@gmail.com>
2019-06-28 00:24:22 +0530

    s

--- src/Nav.js
+++ src/Nav.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const routes = [
-  { to: '/', label: 'Home' },
+  { to: '/', label: 'Puneet Naik' },
   { to: '/about', label: 'About' },
-  { to: '/author', label: 'Author' },
-  { to: '/puneet', label: 'Puneet' }
+  { to: '/author', label: 'Projects' },
+  { to: '/puneet', label: 'More' }
 ];
 
 const Nav = () => {
@@ -13,7 +13,7 @@ const Nav = () => {
     return <NavLink strict exact to={to} key={to}>{label}</NavLink>}
   )
 
-  return <nav className="ml24">{ links }</nav>;
+  return <nav>{ links }</nav>;
 }
 
 export default Nav
--- src/views/Home.js
+++ src/views/Home.js
@@ -3,12 +3,19 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return( 
-      <h1>
-        <div>Sleek</div> 
-        <div className="with--accent">animated</div>
-        <div>views</div>
-        <div>for <span className="with--accent">web apps</span></div>
-      </h1>
+      <div id="particles">
+        <div id="intro">
+          <h2 style={{fontFamily: '"Syncopate", sans-serif', fontWeight: 10}}>JS Developer&nbsp;<a target="_blank" href="deedy.pdf" style={{color: 'white'}}><img src="https://png.icons8.com/office/40/000000/open-resume.png" height={35} width={35} /></a></h2>
+          <hr style={{width: '50%', backgroundColor: '#E83951', borderColor: '#E83951'}} />
+          <a href="https://www.linkedin.com/in/puneet-naik-5a53ab122" style={{color: 'white'}}><i className="fab fa-linkedin" style={{fontSize: '2em'}} /></a>
+          &nbsp;
+          <a href="https://twitter.com/Puneet_Naik" style={{color: 'white'}}><i className="fab fa-twitter" style={{fontSize: '2em'}} /></a>
+          &nbsp;
+          <a href="https://github.com/#/" style={{color: 'white'}}><i className="fab fa-github" style={{fontSize: '2em'}} /></a>
+          &nbsp;
+          <a href="https://medium.com/@#/" style={{color: 'white'}}><i className="fab fa-medium" style={{fontSize: '2em'}} /></a>
+        </div>
+      </div>
     );
   }
 }
